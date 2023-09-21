f0e15afab86d722517a9b8df5fe5052e8bc50fa2
mustufa97 <mustufakhanofficial97@gmail.com>
2023-09-21 21:00:42 +0500

    Mustufa

--- src/Route/Routing.js
+++ src/Route/Routing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from '../pages/Home';
+import AddMember from '../pages/AddMember';
 
 function Routing() {
   return (
@@ -8,6 +9,7 @@ function Routing() {
           <Router>
               <Routes>
                   <Route path="/" Component={Home}/>
+                  <Route path="/AddMember" Component={AddMember}/>
               </Routes>
           </Router>
     </>
---
+++ src/components/MemberDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./assets/css/memberDetails.css"
+
+function MemberDetails() {
+  return (
+    <>
+      <div className="row">
+        <div className="col-lg-8 p-0"></div>
+      </div>
+    </>
+  )
+}
+
+export default MemberDetails
