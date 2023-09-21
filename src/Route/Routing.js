c30f98a0899f69cf8c5966ff075bcb1391ff6be9
abdullaahsaeed <maddaabdullah@gmail.com>
2023-09-21 21:01:59 +0500

    Abdullah

--- src/Route/Routing.js
+++ src/Route/Routing.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from '../pages/Home';
+import Login from '../pages/Login';
 
 function Routing() {
   return (
     <>
           <Router>
               <Routes>
-                  <Route path="/" Component={Home}/>
+                  <Route path="/Home" Component={Home}/>
+                  <Route path="/" Component={Login}/>
               </Routes>
           </Router>
     </>
---
+++ src/components/modal/CreateMember.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateMember() {
+  return (
+    <></>
+  )
+}
+
+export default CreateMember
